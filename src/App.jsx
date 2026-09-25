d0f02db829bb518d547a87f358ed50f5bc659851
princemclein <princemcleinjgonzales@gmail.com>
2026-09-25 14:02:16 +0800

    add fade-in page transition on route change
    - Split routed content into AppRoutes to use useLocation
    - Key <main> by pathname to remount and retrigger animation on nav
    - Add pageFadeIn keyframes (opacity + slight translateY) to global CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import "./App.css";
+
+function AppRoutes() {
+  const location = useLocation();
+
+  return (
+    <main key={location.pathname} className="page-transition">
+      <Routes location={location}>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </main>
+  );
+}
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </main>
+      <AppRoutes />
       <Footer />
     </BrowserRouter>
   );
